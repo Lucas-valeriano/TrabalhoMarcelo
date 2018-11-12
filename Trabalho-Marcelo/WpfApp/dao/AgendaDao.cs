d277cb093b3102f44790f49ed1301b1da4829096
Lucas-valeriano <lucasvalerianosilveira@gmail.com>
2018-11-12 18:45:46 -0200

    Add files via upload

---
+++ Trabalho-Marcelo/WpfApp/dao/AgendaDao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp.model;
+
+namespace WpfApp.dao
+{
+    public class AgendaDao
+    {
+        private readonly Contexto _contexto;
+        public AgendaDao()
+        {
+            _contexto = new Contexto();
+        }
+        public List<Contato> Listar()
+        {
+            try
+            {
+                return _contexto.Contato.ToList();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Falha ao listar produtos!", "Erro", MessageBoxButton.OK, MessageBoxImage.Warning);
+                throw;
+            }
+        }
+
+        public Contato Inserir(Contato contato)
+        {
+            try
+            {
+                var novoContato = _contexto.Contato.Add(contato);
+                _contexto.SaveChanges();
+                MessageBox.Show("Contato cadastrado com sucesso!", "Erro", MessageBoxButton.OK, MessageBoxImage.Information);
+                return novoContato;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Falha ao cadastrar contato!", "Erro", MessageBoxButton.OK, MessageBoxImage.Warning);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Trabalho-Marcelo/WpfApp/model/Contato.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.model
+{
+    public class Contato
+    {
+        public Contato()
+        {
+
+        }
+
+        public Contato(string nome, string email, int idade)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Idade = idade;   
+        }
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public int Idade { get; set; }
+    }
+}
